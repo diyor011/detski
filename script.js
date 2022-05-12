0be54b6b1d74995106074f1d3b3877c308471753
dior011 <diyorraxmatullaev3@gmail.com>
2022-05-12 20:22:36 +0500

    hello

---
+++ script.js
@@ -0,0 +1,11 @@
+let name  = prompt ('ismingizni kiriting')
+let  age  = +prompt ('yilingiz neci')
+let  yaer = +prompt ('xozirgi yilni kiriting')
+function mat(age,year){
+    let rep = year - age 
+    return rep 
+
+}
+alert('salom sizning ismingiz ' + name + ' sizning yoshingiz ' + mat(age,yaer))
+
+
